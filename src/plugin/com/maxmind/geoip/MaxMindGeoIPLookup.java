353aa9862b54ecb805e852a99c150c1fa7abecc5
Daniel Einspanjer <deinspanjer@mozilla.com>
2008-12-09 05:41:20 +0000

    Implemented a very specialized parser for IPv4 numeric addresses.  This is over 1000x times faster than the old way of calling InetAddress.getAddress(), but it means we need another piece of UI to allow the user to decide whether they can use this method or not.  e.g. if they are parsing domain names instead of IPs or if they have IPv6 addresses, or if they have a mix, then they need the old method.

--- src/plugin/com/maxmind/geoip/MaxMindGeoIPLookup.java
+++ src/plugin/com/maxmind/geoip/MaxMindGeoIPLookup.java
@@ -2,7 +2,6 @@
 package plugin.com.maxmind.geoip;
 
 import java.io.IOException;
-
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleValueException;
 import org.pentaho.di.core.row.RowDataUtil;
@@ -35,6 +34,7 @@ public class MaxMindGeoIPLookup extends BaseStep implements StepInterface
 	public MaxMindGeoIPLookup(StepMeta s, StepDataInterface stepDataInterface, int c, TransMeta t, Trans dis)
 	{
 		super(s,stepDataInterface,c,t,dis);
+		initializeIpTokens();
 	}
 	
 	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
@@ -77,7 +77,7 @@ public class MaxMindGeoIPLookup extends BaseStep implements StepInterface
 
 		String ip = data.outputRowMeta.getString(r, data.ipAddressFieldIndex);
         
-        Location location = data.lookupService.getLocation(ip);
+        Location location = data.lookupService.getLocation(getAddressFromIpV4(ip));
         
         if (location != null)
         {
@@ -96,8 +96,70 @@ public class MaxMindGeoIPLookup extends BaseStep implements StepInterface
 			
 		return true;
 	}
-		
-	public boolean init(StepMetaInterface smi, StepDataInterface sdi)
+
+    private static short[][][] ipTokens = new short[10][10][10];
+
+    private static void initializeIpTokens() {
+        for (short s = 0; s < 256; s++) {
+            String ss = String.valueOf(s);
+            switch (ss.length()) {
+                case 1:
+                    ipTokens[0][0][ss.charAt(0)-48] = s;
+                    break;
+                case 2:
+                    ipTokens[0][ss.charAt(0)-48][ss.charAt(1)-48] = s;
+                    break;
+                case 3:
+                    ipTokens[ss.charAt(0)-48][ss.charAt(1)-48][ss.charAt(2)-48] = s;
+            }
+        }
+    }
+
+	/**
+	 * This is a heavily optimized method for extracting the long representation of the IP
+	 * address without creating several Regex or String objects for each call.
+	 * @param ip
+	 * @return
+	 */
+	private long getAddressFromIpV4(String ip)
+    {
+        long retval = 0;
+	    if (ip == null) return 0;
+	    
+        short parsedToken;
+        int start = 0;
+        int next = 0;
+
+        for (int i = 0; i < 4; i++) {
+            next = ip.indexOf('.', start);
+            if (next == -1) {
+                next = ip.length() - start;
+            }
+            try {
+                switch (next - start) {
+                    case 1:
+                        parsedToken = ipTokens[0][0][ip.charAt(start)-48];
+                        break;
+                    case 2:
+                        parsedToken = ipTokens[0][ip.charAt(start)-48][ip.charAt(start + 1)-48];
+                        break;
+                    case 3:
+                        parsedToken = ipTokens[ip.charAt(start)-48][ip.charAt(start + 1)-48][ip.charAt(start + 2)-48];
+                        break;
+                    default:
+                        return 0;
+                }
+            } catch (IndexOutOfBoundsException e) {
+                return 0;
+            }
+
+	        retval += parsedToken << ((3-i)*8);
+	    }
+	    
+	    return retval;
+    }
+
+    public boolean init(StepMetaInterface smi, StepDataInterface sdi)
 	{
 	    meta = (MaxMindGeoIPLookupMeta)smi;
 	    data = (MaxMindGeoIPLookupData)sdi;
