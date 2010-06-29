96ace608c8ed584980fe47474add8066770f57eb
Daniel Einspanjer <deinspanjer@mozilla.com>
2010-06-29 16:16:50 +0000

    Upgrades to make the new version 4.0 compatible

--- src/plugin/com/maxmind/geoip/MaxMindGeoIPLookup.java
+++ src/plugin/com/maxmind/geoip/MaxMindGeoIPLookup.java
@@ -1,11 +1,8 @@
-
 package plugin.com.maxmind.geoip;
 
-import java.io.IOException;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleValueException;
 import org.pentaho.di.core.row.RowDataUtil;
-import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStep;
@@ -14,143 +11,96 @@ import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
 
-import com.maxmind.geoip.Location;
-import com.maxmind.geoip.regionName;
-import com.maxmind.geoip.timeZone;
-
-
-
-
 /*
- * @author Daniel Einspanjer
+ * @author Daniel Einspanjer and Doug Moran
  * @since  April-16-2008
  */
 
 public class MaxMindGeoIPLookup extends BaseStep implements StepInterface
 {
-    private MaxMindGeoIPLookupData data;
-	private MaxMindGeoIPLookupMeta meta;
-	
+  private MaxMindGeoIPLookupData data;
+  private MaxMindGeoIPLookupMeta meta;
+	private MaxMindDatabase maxMindDatabase;
+
 	public MaxMindGeoIPLookup(StepMeta s, StepDataInterface stepDataInterface, int c, TransMeta t, Trans dis)
 	{
-		super(s,stepDataInterface,c,t,dis);
-	}
-	
-	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
-	{
-	    meta = (MaxMindGeoIPLookupMeta)smi;
-	    data = (MaxMindGeoIPLookupData)sdi;
-	    
-		Object[] r=getRow();    // get row, blocks when needed!
-		if (r==null)  // no more input to be expected...
-		{
-			setOutputDone();
-			return false;
-		}
-        
-        if (first)
-        {
-            first = false;
-            
-            data.firstNewFieldIndex = getInputRowMeta().size();
-            data.outputRowMeta = (RowMetaInterface)getInputRowMeta().clone();
-            meta.getFields(data.outputRowMeta, getStepname(), null, null, this);            
+    super(s, stepDataInterface, c, t, dis);
+  }
 
-			// search field
-			data.ipAddressFieldIndex = data.outputRowMeta.indexOfValue(meta.getIpAddressFieldName());
+  public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
+  {
+    meta = (MaxMindGeoIPLookupMeta) smi;
+    data = (MaxMindGeoIPLookupData) sdi;
 
-			// only String type allowed
-			if (!data.outputRowMeta.getValueMeta(data.ipAddressFieldIndex).isString())
-			{
-				throw new KettleValueException((Messages.getString("MaxMindGeoIPLookup.Log.IpAddressFieldNotValid",meta.getIpAddressFieldName()))); //$NON-NLS-1$ //$NON-NLS-2$
-			}
-        }
-        
-		// reserve room
-		Object[] outputRow = RowDataUtil.allocateRowData(data.outputRowMeta.size());
-				
-		for (int i = 0; i < data.firstNewFieldIndex; i++)
-		{
-			outputRow[i] = r[i];
-		}
-
-		String ip = data.outputRowMeta.getString(r, data.ipAddressFieldIndex);
-        
-        Location location = data.lookupService.getLocation(getAddressFromIpV4(ip));
-        
-        if (location != null)
-        {
-        	outputRow[data.firstNewFieldIndex]   = location.countryCode;
-        	outputRow[data.firstNewFieldIndex+1] = location.countryName;
-        	outputRow[data.firstNewFieldIndex+2] = location.region;
-        	outputRow[data.firstNewFieldIndex+3] = regionName.regionNameByCode(location.countryCode, location.region);
-        	outputRow[data.firstNewFieldIndex+4] = location.city;
-        	outputRow[data.firstNewFieldIndex+5] = (double)location.latitude;
-        	outputRow[data.firstNewFieldIndex+6] = (double)location.longitude;
-        	outputRow[data.firstNewFieldIndex+7] = timeZone.timeZoneByCountryAndRegion(location.countryCode, location.region);
-        }
-		putRow(data.outputRowMeta, outputRow);     // copy row to possible alternate rowset(s).
-
-		if (checkFeedback(getLinesRead())) logBasic("Linenr "+getLinesRead());  // Some basic logging every 5000 rows.
-			
-		return true;
-	}
-
-	/**
-	 * This is a heavily optimized method for extracting the long representation of the IP
-	 * address without creating several Regex or String objects for each call.
-	 * @param ip
-	 * @return
-	 */
-	private long getAddressFromIpV4(String ip)
+    Object[] r = getRow(); // get row, blocks when needed!
+    if (r == null) // no more input to be expected...
     {
-	    if (ip == null) return 0;
-        int length = ip.length();
-        long result = 0;
-        short blockNumber = 0;
-        long block = 0;
-        for (int i = 0; i < length; i++) {
-            char c = ip.charAt(i);
-            if (c == '.') {
-                result += block << ((3 - blockNumber) * 8);
-                blockNumber++;
-                block = 0;
-            } else {
-                block = block * 10 + c - '0';
-            }
-        }
-        result += block << ((3 - blockNumber) * 8);
-        return result;
+      setOutputDone();
+      return false;
     }
 
-    public boolean init(StepMetaInterface smi, StepDataInterface sdi)
-	{
-	    meta = (MaxMindGeoIPLookupMeta)smi;
-	    data = (MaxMindGeoIPLookupData)sdi;
+    if (first)
+    {
+      first = false;
+      
+      data.firstNewFieldIndex = getInputRowMeta().size();
+      data.outputRowMeta = getInputRowMeta().clone();
+      
+      data.ipAddressFieldIndex = data.outputRowMeta.indexOfValue(meta.getIpAddressFieldName());
 
-	    if (super.init(smi, sdi))
-	    {
-	    	try
-	    	{
-	    		data.initLocationService(meta.getDbLocation());
-			}
-	    	catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-	    	return true;
-	    }
-	    else
-	    {
-	    	return false;
-	    }
-	}
+      maxMindDatabase = meta.getMaxMindDatabase();  // Just to be sure nothing changed
+      meta.getFields(data.outputRowMeta, getStepname(), null, null, this);
 
-	public void dispose(StepMetaInterface smi, StepDataInterface sdi)
-	{
-	    meta = (MaxMindGeoIPLookupMeta)smi;
-	    data = (MaxMindGeoIPLookupData)sdi;
+      // only String type allowed
+      if (!data.outputRowMeta.getValueMeta(data.ipAddressFieldIndex).isString())
+      {
+        throw new KettleValueException((Messages.getString("MaxMindGeoIPLookup.Log.IpAddressFieldNotValid",meta.getIpAddressFieldName()))); //$NON-NLS-1$ 
+      }
+    }
 
-	    super.dispose(smi, sdi);
-	}
+    // reserve room
+    Object[] outputRow = RowDataUtil.allocateRowData(data.outputRowMeta.size());
+
+    for (int i = 0; i < data.firstNewFieldIndex; i++)
+    {
+      outputRow[i] = r[i];
+    }
+
+    maxMindDatabase.getRowData(outputRow, data.firstNewFieldIndex, data.outputRowMeta.getString(r, data.ipAddressFieldIndex) );
+    
+    putRow(data.outputRowMeta, outputRow); // copy row to possible alternate rowset(s).
+
+    if (checkFeedback(getLinesRead())) logBasic("Linenr " + getLinesRead()); // Some basic logging every 5000 rows.
+
+    return true;
+  }
+
+
+  public boolean init(StepMetaInterface smi, StepDataInterface sdi) 
+  {
+    meta = (MaxMindGeoIPLookupMeta) smi;
+    data = (MaxMindGeoIPLookupData) sdi;
+
+    if (super.init(smi, sdi))
+    {
+        maxMindDatabase = meta.getMaxMindDatabase();
+        if ( maxMindDatabase == null )
+        {
+          return false;
+        }
+      return true;
+    } 
+    else 
+    {
+      return false;
+    }
+  }
+
+  public void dispose(StepMetaInterface smi, StepDataInterface sdi)
+  {
+    meta = (MaxMindGeoIPLookupMeta) smi;
+    data = (MaxMindGeoIPLookupData) sdi;
+
+    super.dispose(smi, sdi);
+  }
 }
--- src/plugin/com/maxmind/geoip/MaxMindGeoIPLookupDialog.java
+++ src/plugin/com/maxmind/geoip/MaxMindGeoIPLookupDialog.java
@@ -1,52 +1,72 @@
 /*
- * @author Daniel Einspanjer
+ * @author Daniel Einspanjer and Doug Moran
  * @since  April-16-2008
+ * 
  */
 
 package plugin.com.maxmind.geoip;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.ShellAdapter;
 import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.pentaho.di.core.Const;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStepMeta;
 import org.pentaho.di.trans.step.StepDialogInterface;
+import org.pentaho.di.ui.core.dialog.ErrorDialog;
 import org.pentaho.di.ui.core.widget.ColumnInfo;
 import org.pentaho.di.ui.core.widget.TableView;
+import org.pentaho.di.ui.core.widget.TextVar;
 import org.pentaho.di.ui.trans.step.BaseStepDialog;
 
 public class MaxMindGeoIPLookupDialog extends BaseStepDialog implements StepDialogInterface
 {
-	private Label        wlIpAddressFieldName;
-	private Text         wIpAddressFieldName;
-	private FormData     fdlIpAddressFieldName, fdIpAddressFieldName;
+  private Label        wlFieldname;
+  private CCombo       wFieldname;
+  private FormData     fdlFieldname, fdFieldname;
 
-	private Label        wlDbLocation;
-	private Text         wDbLocation;
-	private FormData     fdlDbLocation, fdDbLocation;
-	
+  private Label        wlDbType;
+  private CCombo       wDbType;
+  private FormData     fdlDbType;
+
+  private TextVar      wFilename;
+  private Button       wbbFilename; // Browse for a file
+  
+  private Button       wbDbInfo;
+  private Label        wlDbInfo;
+  private FormData     fdlDbInfo, fdDbInfo;
+  
 	private Label        wlFields;
 	private TableView    wFields;
 	private FormData     fdlFields, fdFields;
 
+	private ColumnInfo[] colinf;
 	private MaxMindGeoIPLookupMeta  input;
-
+	private boolean gotPreviousFields=false;
+	
 	public MaxMindGeoIPLookupDialog(Shell parent, Object in, TransMeta transMeta, String sname)
 	{
 		super(parent, (BaseStepMeta)in, transMeta, sname);
@@ -99,52 +119,106 @@ public class MaxMindGeoIPLookupDialog extends BaseStepDialog implements StepDial
 		fdStepname.top  = new FormAttachment(0, margin);
 		fdStepname.right= new FormAttachment(100, 0);
 		wStepname.setLayoutData(fdStepname);
+    Control lastControl = wStepname;
 
-		// Typefield line
-		wlIpAddressFieldName=new Label(shell, SWT.RIGHT);
-		wlIpAddressFieldName.setText(Messages.getString("MaxMindGeoIPLookupDialog.IPAddressFieldName.Label")); //$NON-NLS-1$
- 		props.setLook(wlIpAddressFieldName);
-		fdlIpAddressFieldName=new FormData();
-		fdlIpAddressFieldName.left = new FormAttachment(0, 0);
-		fdlIpAddressFieldName.right= new FormAttachment(middle, -margin);
-		fdlIpAddressFieldName.top  = new FormAttachment(wStepname, margin);
-		wlIpAddressFieldName.setLayoutData(fdlIpAddressFieldName);
-		wIpAddressFieldName=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-		wIpAddressFieldName.setText(""); //$NON-NLS-1$
- 		props.setLook(wIpAddressFieldName);
-		wIpAddressFieldName.addModifyListener(lsMod);
-		fdIpAddressFieldName=new FormData();
-		fdIpAddressFieldName.left = new FormAttachment(middle, 0);
-		fdIpAddressFieldName.top  = new FormAttachment(wStepname, margin);
-		fdIpAddressFieldName.right= new FormAttachment(100, 0);
-		wIpAddressFieldName.setLayoutData(fdIpAddressFieldName);
+    // Fieldname line
+    wlFieldname=new Label(shell, SWT.RIGHT);
+    wlFieldname.setText("Incoming IP address field to lookup"); //$NON-NLS-1$
+    wlFieldname.setToolTipText("IP Address field must be a String type (Only String fields are shown)"); //$NON-NLS-1$
+    props.setLook(wlFieldname);
+    fdlFieldname=new FormData();
+    fdlFieldname.left = new FormAttachment(0, 0);
+    fdlFieldname.right= new FormAttachment(middle, -margin);
+    fdlFieldname.top  = new FormAttachment(lastControl, margin);
+    wlFieldname.setLayoutData(fdlFieldname);
 
-		// Typefield line
-		wlDbLocation=new Label(shell, SWT.RIGHT);
-		wlDbLocation.setText(Messages.getString("MaxMindGeoIPLookupDialog.DBLocation.Label")); //$NON-NLS-1$
-		wlDbLocation.setToolTipText(Messages.getString("MaxMindGeoIPLookupDialog.DBLocation.Tooltip"));
- 		props.setLook(wlDbLocation);
-		fdlDbLocation=new FormData();
-		fdlDbLocation.left = new FormAttachment(0, 0);
-		fdlDbLocation.right= new FormAttachment(middle, -margin);
-		fdlDbLocation.top  = new FormAttachment(wIpAddressFieldName, margin);
-		wlDbLocation.setLayoutData(fdlDbLocation);
-		wDbLocation=new Text(shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-		wDbLocation.setText(""); //$NON-NLS-1$
- 		props.setLook(wDbLocation);
-		wDbLocation.addModifyListener(lsMod);
-		fdDbLocation=new FormData();
-		fdDbLocation.left = new FormAttachment(middle, 0);
-		fdDbLocation.top  = new FormAttachment(wIpAddressFieldName, margin);
-		fdDbLocation.right= new FormAttachment(100, 0);
-		wDbLocation.setLayoutData(fdDbLocation);
+    wFieldname=new CCombo(shell, SWT.BORDER | SWT.READ_ONLY);
+    props.setLook(wFieldname);
+    wFieldname.addModifyListener(lsMod);
+    fdFieldname=new FormData();
+    fdFieldname.left = new FormAttachment(middle, 0);
+    fdFieldname.top  = new FormAttachment(lastControl, margin);
+    fdFieldname.right= new FormAttachment(100, 0);
+    wFieldname.setLayoutData(fdFieldname);
+    lastControl = wFieldname;
+    
+    // Filename...
+    // The filename browse button
+    wbbFilename = new Button(shell, SWT.PUSH | SWT.CENTER);
+    props.setLook(wbbFilename);
+    wbbFilename.setText(Messages.getString("System.Button.Browse")); //$NON-NLS-1$
+    wbbFilename.setToolTipText(Messages.getString("System.Tooltip.BrowseForFileOrDirAndAdd")); //$NON-NLS-1$
+    FormData fdbFilename = new FormData();
+    fdbFilename.top = new FormAttachment(lastControl, margin);
+    fdbFilename.right = new FormAttachment(100, 0);
+    wbbFilename.setLayoutData(fdbFilename);
 
-		wlFields=new Label(shell, SWT.RIGHT);
+    // The field itself...
+    Label wlFilename = new Label(shell, SWT.RIGHT);
+    wlFilename.setText("Maxmind DB File");
+    props.setLook(wlFilename);
+    FormData fdlFilename = new FormData();
+    fdlFilename.top = new FormAttachment(lastControl, margin);
+    fdlFilename.left = new FormAttachment(0, 0);
+    fdlFilename.right = new FormAttachment(middle, -margin);
+    wlFilename.setLayoutData(fdlFilename);
+    wFilename = new TextVar(transMeta, shell, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
+    props.setLook(wFilename);
+    wFilename.addModifyListener(lsMod);
+    FormData fdFilename = new FormData();
+    fdFilename.top = new FormAttachment(lastControl, margin);
+    fdFilename.left = new FormAttachment(middle, 0);
+    fdFilename.right = new FormAttachment(wbbFilename, -margin);
+    wFilename.setLayoutData(fdFilename);
+    lastControl = wFilename;
+
+    // DB Type
+    wlDbType=new Label(shell, SWT.RIGHT);
+    wlDbType.setText("Maxmind Database Type"); //$NON-NLS-1$
+    props.setLook(wlDbType);
+    fdlDbType=new FormData();
+    fdlDbType.left = new FormAttachment(0, 0);
+    fdlDbType.right= new FormAttachment(middle, -margin);
+    fdlDbType.top  = new FormAttachment(lastControl, margin);
+    wlDbType.setLayoutData(fdlDbType);
+
+    wDbType=new CCombo(shell, SWT.BORDER | SWT.READ_ONLY);
+    props.setLook(wDbType);
+    wDbType.addModifyListener(lsMod);
+    fdlDbType=new FormData();
+    fdlDbType.left = new FormAttachment(middle, 0);
+    fdlDbType.top  = new FormAttachment(lastControl, margin);
+    fdlDbType.right= new FormAttachment(100, 0);
+    wDbType.setLayoutData(fdlDbType);
+    wDbType.setItems( MaxMindGeoIP.getDatabaseTypes() );
+    lastControl = wDbType;
+    
+    // DBInfo line
+    wbDbInfo=new Button(shell, SWT.PUSH| SWT.CENTER);
+    wbDbInfo.setText("Get DB Info"); //$NON-NLS-1$
+    wbDbInfo.setToolTipText("Examine the DB File and describe it");
+    props.setLook(wbDbInfo);
+    fdlDbInfo=new FormData();
+    fdlDbInfo.left = new FormAttachment(0, 0);
+    fdlDbInfo.right= new FormAttachment(middle, -margin);
+    fdlDbInfo.top  = new FormAttachment(lastControl, margin);
+    wbDbInfo.setLayoutData(fdlDbInfo);
+    wlDbInfo=new Label(shell, SWT.LEFT);
+    wlDbInfo.setText("Hi Dude"); //$NON-NLS-1$
+    props.setLook(wlDbInfo);
+    fdDbInfo=new FormData();
+    fdDbInfo.left = new FormAttachment(middle, 0);
+    fdDbInfo.top  = new FormAttachment(lastControl, margin);
+    fdDbInfo.right= new FormAttachment(100, 0);
+    wlDbInfo.setLayoutData(fdDbInfo);
+    lastControl = wbDbInfo;
+    
+    wlFields=new Label(shell, SWT.RIGHT);
 		wlFields.setText(Messages.getString("MaxMindGeoIPLookupDialog.Fields.Label")); //$NON-NLS-1$
  		props.setLook(wlFields);
 		fdlFields=new FormData();
 		fdlFields.left = new FormAttachment(0, 0);
-		fdlFields.top  = new FormAttachment(wDbLocation, margin);
+		fdlFields.top  = new FormAttachment(lastControl, margin);
 		wlFields.setLayoutData(fdlFields);
 
 		wOK=new Button(shell, SWT.PUSH);
@@ -156,21 +230,21 @@ public class MaxMindGeoIPLookupDialog extends BaseStepDialog implements StepDial
 
 		//TODO: Support list of fields to add
 		//TODO: Need an enum of geoip data types
-		final int fieldsRows = 0;
+    final int fieldsRows = 0;
 
-        final ColumnInfo[] colinf = new ColumnInfo[] {
-                new ColumnInfo(
-                        Messages.getString("MaxMindGeoIPLookupDialog.ColumnInfo.NewField"), ColumnInfo.COLUMN_TYPE_TEXT, false), //$NON-NLS-1$
-                new ColumnInfo(
-                        Messages.getString("MaxMindGeoIPLookupDialog.ColumnInfo.LookupType"), ColumnInfo.COLUMN_TYPE_CCOMBO, new String[] {"TODO"}), //$NON-NLS-1$
-                new ColumnInfo(
-                        Messages.getString("MaxMindGeoIPLookupDialog.ColumnInfo.IfNull"), ColumnInfo.COLUMN_TYPE_TEXT, false)}; //$NON-NLS-1$
-        wFields = new TableView(transMeta, shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI, colinf, fieldsRows,
-                lsMod, props);
+    colinf = new ColumnInfo[] {
+        new ColumnInfo(
+            Messages.getString("MaxMindGeoIPLookupDialog.ColumnInfo.NewField"), ColumnInfo.COLUMN_TYPE_TEXT, false), //$NON-NLS-1$
+        new ColumnInfo(
+            Messages.getString("MaxMindGeoIPLookupDialog.ColumnInfo.LookupType"), ColumnInfo.COLUMN_TYPE_CCOMBO, new String[] { "" }), //$NON-NLS-1$ //$NON-NLS-2$
+        new ColumnInfo(
+            Messages.getString("MaxMindGeoIPLookupDialog.ColumnInfo.IfNull"), ColumnInfo.COLUMN_TYPE_TEXT, false) }; //$NON-NLS-1$
+    wFields = new TableView(transMeta, shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI, colinf, fieldsRows, lsMod,
+        props);
 
-		fdFields=new FormData();
-		fdFields.left = new FormAttachment(0, 0);
-		fdFields.top  = new FormAttachment(wlFields, margin);
+    fdFields = new FormData();
+    fdFields.left = new FormAttachment(0, 0);
+    fdFields.top = new FormAttachment(wlFields, margin);
 		fdFields.right  = new FormAttachment(100, 0);
 		fdFields.bottom = new FormAttachment(wOK, -2*margin);
 		wFields.setLayoutData(fdFields);
@@ -189,6 +263,56 @@ public class MaxMindGeoIPLookupDialog extends BaseStepDialog implements StepDial
 		// Detect X or ALT-F4 or something that kills this window...
 		shell.addShellListener(	new ShellAdapter() { public void shellClosed(ShellEvent e) { cancel(); } } );
 
+    // Lookup field Selector
+    wFieldname.addFocusListener(new FocusListener() {
+        public void focusLost(org.eclipse.swt.events.FocusEvent e){}
+    
+        public void focusGained(org.eclipse.swt.events.FocusEvent e) {
+            Cursor busy = new Cursor(shell.getDisplay(), SWT.CURSOR_WAIT);
+            shell.setCursor(busy);
+            getInputFields();
+            shell.setCursor(null);
+            busy.dispose();
+        }
+      }
+    );
+      
+    // DBTYPE field Selector - set the field selections when DB type changes
+    wDbType.addModifyListener(new ModifyListener() {
+      public void modifyText(ModifyEvent arg0) {
+        colinf[1].setComboValues( MaxMindGeoIP.getDbFieldanmes( wDbType.getText() ) );
+      }
+    } );
+      
+    // Listen to the browse button next to the file name
+    wbbFilename.addSelectionListener(new SelectionAdapter() {
+        public void widgetSelected(SelectionEvent e) {
+          FileDialog dialog = new FileDialog(shell, SWT.OPEN);
+          dialog.setFilterExtensions(new String[] { "*.dat", "*" });  //$NON-NLS-1$//$NON-NLS-2$
+          dialog.setFilterNames(new String[] { "Maxmind GeoIP data", Messages.getString("System.FileType.AllFiles") }); //$NON-NLS-2$
+
+          if (wFilename.getText() != null) {
+            String fname = transMeta.environmentSubstitute(wFilename.getText());
+            dialog.setFileName(fname);
+          }
+  
+          if (dialog.open() != null) {
+            String str = dialog.getFilterPath() + System.getProperty("file.separator") + dialog.getFileName(); //$NON-NLS-1$
+            wFilename.setText(str);
+            updateDbInfo();
+          }
+        }
+      }
+    );
+
+    // Get info about the selected database
+    wbDbInfo.addSelectionListener(new SelectionAdapter() {
+        public void widgetSelected(SelectionEvent se) {
+          updateDbInfo();
+        }
+      }
+    );
+
 		// Set the shell size, based upon previous time...
 		setSize();
 		
@@ -204,25 +328,68 @@ public class MaxMindGeoIPLookupDialog extends BaseStepDialog implements StepDial
 		return stepname;
 	}
 	
-	public void getData()
-	{	
-		int i;
-		
-		if (input.getIpAddressFieldName()!=null) wIpAddressFieldName.setText(input.getIpAddressFieldName());
-		if (input.getDbLocation()!=null) wDbLocation.setText(input.getDbLocation());
-		
-        for (i = 0; i < input.getFieldName().length; i++)
-		{
-            final TableItem ti = wFields.table.getItem(i);
-            if (input.getFieldName()[i] != null) ti.setText(1, input.getFieldName()[i]);
-            ti.setText(2, input.getFieldLookupType()[i]);
-            if (input.getFieldIfNull()[i] != null) ti.setText(3, input.getFieldIfNull()[i]);
-		}
-		wFields.setRowNums();
-		wFields.optWidth(true);
+  public void getData() 
+  {
 
-		wStepname.selectAll();
+    if (input.getIpAddressFieldName() != null)
+      wFieldname.setText(input.getIpAddressFieldName());
+    if (input.getDbLocation() != null) {
+      wFilename.setText(input.getDbLocation());
+      updateDbInfo();
+    }
+    if (input.getDbType() != null) {
+      wDbType.setText(input.getDbType());
+    }
+    
+    
+    Table table = wFields.table;
+    if (input.getFieldName().length>0) table.removeAll();
+    for (int i=0;i<input.getFieldName().length;i++)
+    {
+      TableItem ti = new TableItem(table, SWT.NONE);
+      ti.setText(0, ""+(i+1)); //$NON-NLS-1$
+      ti.setText(1, (input.getFieldName()[i] == null) ? "" : input.getFieldName()[i] ); //$NON-NLS-1$
+      ti.setText(2, (input.getFieldLookupType()[i] == null) ? "" : input.getFieldLookupType()[i] ); //$NON-NLS-1$
+      ti.setText(3, (input.getFieldIfNull()[i] == null) ? "" : input.getFieldIfNull()[i] ); //$NON-NLS-1$
+    }
+    wFields.setRowNums();
+    wFields.optWidth(true);
+
+    wStepname.selectAll();
+  }
+
+	private void updateDbInfo() {
+    wlDbInfo.setText( MaxMindGeoIP.getDbInfo( wFilename.getText() ) );
 	}
+	
+  private void getInputFields()
+  {
+   if(!gotPreviousFields)
+   {
+     gotPreviousFields=true;
+    try
+    {
+      String fieldname=wFieldname.getText();
+      
+      wFieldname.removeAll();
+      RowMetaInterface r = transMeta.getPrevStepFields(stepname);
+      if (r!=null)
+      { // Only let them select the string fields
+        for ( int i = 0; i < r.size(); ++i ) {
+           if ( r.getValueMeta(i).isString() ) {
+             wFieldname.add( r.getValueMeta(i).getName() );
+           }
+        }
+        if(fieldname!=null) wFieldname.setText(fieldname);
+      }
+    }
+    catch(KettleException ke)
+    {
+      new ErrorDialog(shell, Messages.getString("ValueMapperDialog.FailedToGetFields.DialogTitle"), Messages.getString("ValueMapperDialog.FailedToGetFields.DialogMessage"), ke); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+   }
+  }
+
 	
 	private void cancel()
 	{
@@ -231,28 +398,29 @@ public class MaxMindGeoIPLookupDialog extends BaseStepDialog implements StepDial
 		dispose();
 	}
 	
-	private void ok()
-	{
-		if (Const.isEmpty(wStepname.getText())) return;
+  private void ok()
+  {
+  	if (Const.isEmpty(wStepname.getText())) return;
 
-		stepname = wStepname.getText(); // return value
+    stepname = wStepname.getText(); // return value
 
-		input.setIpAddressFieldName( wIpAddressFieldName.getText() );
-		input.setDbLocation( wDbLocation.getText() );
-		
-		//Table table = wFields.table;
-		int nrfields = wFields.nrNonEmpty();
-		
-		input.allocate(nrfields);
-		
-        for (int i = 0; i < input.getFieldName().length; i++)
-		{
-            final TableItem ti = wFields.getNonEmpty(i);
-            input.getFieldName()[i] = ti.getText(1);
-			input.getFieldLookupType()[i] = ti.getText( 2 );
-            input.getFieldIfNull()[i] = ti.getText(3);
-		}
-		
-		dispose();
-	}
+    input.setIpAddressFieldName(wFieldname.getText());
+    input.setDbLocation(wFilename.getText());
+    input.setDbType(wDbType.getText());
+
+    //Table table = wFields.table;
+    int nrfields = wFields.nrNonEmpty();
+
+    input.allocate(nrfields);
+
+    for (int i = 0; i < input.getFieldName().length; i++)
+    {
+      final TableItem ti = wFields.getNonEmpty(i);
+      input.getFieldName()[i] = ti.getText(1);
+      input.getFieldLookupType()[i] = ti.getText(2);
+      input.getFieldIfNull()[i] = ti.getText(3);
+    }
+
+    dispose();
+  }
 }
