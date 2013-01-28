cddf24e0d24dd8cfd3df10f265ed91aa880489ea
Pedro Alves <pmgalves@gmail.com>
2013-01-28 16:28:17 +0000

    - Added VFS support to maxmind geoip step; - Some sanitizing done on build.xml; - Upgraded geoip to current version, one commit after 1.2.7

--- src/com/maxmind/geoip/MaxMindGeoIP.java
+++ src/com/maxmind/geoip/MaxMindGeoIP.java
@@ -1,13 +1,19 @@
 package com.maxmind.geoip;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
+import org.apache.commons.vfs.FileName;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.FileType;
+import org.apache.commons.vfs.Selectors;
+import org.apache.commons.vfs.VFS;
 
 import org.pentaho.di.core.Const;
+import sun.java2d.pipe.SpanClipRenderer;
 
-import com.maxmind.geoip.DatabaseInfo;
-import com.maxmind.geoip.LookupService;
 /*
  * The Overall management class for all the MaxMind databases
  */
@@ -23,6 +29,8 @@ public class MaxMindGeoIP {
   private static final String DB_ISP = "ISP"; //$NON-NLS-1$
   private static final String DB_ORG = "ORG"; //$NON-NLS-1$
   private static final String DB_DOMAIN = "DOMAIN"; //$NON-NLS-1$
+  private static FileSystemManager fsManager = null;
+  private static boolean vfsEnabled = true;
 
   private static final String[] dbTypes = { DB_CITY, DB_COUNTRY, DB_ISP, DB_ORG, DB_DOMAIN };
   
@@ -68,15 +76,61 @@ public class MaxMindGeoIP {
 
   /**
    * Let's just keep one copy of each database type in memory (avoid, multiple caches).
+   * We're adding support for VFS in this step. The maxmindgeoip.jar requires random access
+   * to the file, so we can't add there proper support. If it's a vfs path, we'll
+   * copy the file to the tmp folder
    * @param dbLocation The path to the binary database
    * @return a reference to the global instance of the lookup service
    * @throws IOException
    */
   public static final synchronized LookupService initLookupService(String dbLocation) throws IOException {
+    
+      
+
+    if(fsManager == null){
+        fsManager = VFS.getManager();
+    }
+
     LookupService ls = null;
     WeakReference<LookupService> wrLs = globalLookupServices.get(dbLocation);
     if ((wrLs == null) || ((ls = wrLs.get()) == null)) {
-      ls = new LookupService(dbLocation, LookupService.GEOIP_MEMORY_CACHE);  // Cache here for runtime
+        
+        String localDbLocation = dbLocation;
+
+        try {
+
+            FileName dbVfs = fsManager.resolveURI(dbLocation);
+            if (dbVfs.getScheme().equals("file")) {
+                // Do nothing
+            } else {
+                // It's remote - copy it locally
+
+                FileObject source = fsManager.resolveFile(dbLocation);
+                if (source.exists() && source.getType().equals(FileType.FILE) && source.isReadable()) {
+
+                    // copy to a tmp file, that will be deleted in the end
+                    File localDbFile = File.createTempFile(dbVfs.getBaseName(), "." + dbVfs.getExtension());
+                    localDbFile.deleteOnExit();
+                    FileObject localDbFileObject = fsManager.resolveFile(localDbFile.getAbsolutePath());
+                    localDbFileObject.copyFrom(source, Selectors.SELECT_SELF);
+
+                    // Closing references - we don't need them
+                    source.close();
+                    localDbFileObject.close();
+
+                    
+                    // this.log(Level.INFO, "Successfully copied file from " + dbLocation + " to " + localDbFile.getAbsolutePath());
+                    localDbLocation = localDbFile.getAbsolutePath();
+                }
+
+
+            }
+        } catch (Exception e) {
+            // ignore and keep going with local access
+        }
+
+      // Logger.getLogger(MaxMindGeoIP.class.getName()).log(Level.INFO, "GeoIP using database " + localDbLocation);
+      ls = new LookupService(localDbLocation, LookupService.GEOIP_MEMORY_CACHE);  // Cache here for runtime
       globalLookupServices.put(dbLocation, new WeakReference<LookupService>(ls));
     }
     return ls;
